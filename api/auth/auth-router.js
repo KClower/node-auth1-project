bf33ec86f6a08f1774df9d4f5af3630e8c43807d
KClower <kclower3@gmail.com>
2024-08-07 15:53:44 -0500

    finished project

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,3 +1,18 @@
+
+bcryptjs = require('bcryptjs');
+
+const router = require('express').Router()
+
+const {
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength
+} = require('./auth-middleware.js');
+
+const Users = require('../users/users-model.js');
+const { knexStore } = require('../store-config.js');
+
+
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
 
@@ -24,7 +39,27 @@
     "message": "Password must be longer than 3 chars"
   }
  */
+router.post('/register',
+  checkUsernameFree,
+  checkPasswordLength,
+  (req, res) => {
+    let registerUser = req.body;
 
+    const rounds = process.env.HASH_ROUNDS || 4;
+
+    const hash = bcryptjs.hashSync(registerUser.password, rounds);
+
+    registerUser.password = hash;
+
+    Users.add(registerUser)
+      .then(saved => {
+        return res.status(201).json({ data: saved });
+      })
+      .catch(error => {
+        console.log(error);
+        return res.status(500).json({ message: "There was an error while saving the user to the database" });
+      });
+  })
 
 /**
   2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
@@ -41,7 +76,28 @@
     "message": "Invalid credentials"
   }
  */
+router.post('/login', checkUsernameExists, (req, res) => {
+  const { username, password } = req.body;
 
+  Users.findBy({ username })
+    .then(users => {
+      const user = users[0];
+
+      if (user && bcryptjs.compareSync(password, user.password)) {
+
+        req.session.username = user.username;
+        req.session.user = true;
+
+        return res.status(200).json({ message: `Welcome ${username}`, session: req.session });
+
+      } else {
+        return res.status(401).json({ message: "Invalid credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: error.message });
+    });
+});
 
 /**
   3 [GET] /api/auth/logout
@@ -58,6 +114,33 @@
     "message": "no session"
   }
  */
+// router.get('/logout', (req, res) => {
+//   if (req.session) {
+//     req.session.destroy()
+//     return res.status(200).json({ message: "logged out" })
+//   }
+//   if (!req.session) {
+//     return res.status(200).json({ message: "no session" })
+//   }
+// })
 
- 
+router.get('/logout', (req, res) => {
+  if (req.session && req.session.user) {
+    knexStore.destroy(req.sessionID)
+    req.session.destroy(err => {
+      if (err) {
+        res.status(500).json({ Message: "error logging out, please try again later" })
+      } else {
+        res.clearCookie("chocolatechip")
+        res.status(204).end()
+      }
+    })
+
+  } else {
+    res.status(200).json({ Message: "already logged out" })
+  }
+
+});
+
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const bcryptjs = require('bcryptjs');
+const session = require("express-session");
+const { knexStore } = require('./store-config.js')
+
+const authRouter = require('./auth/auth-router.js');
+const usersRouter = require('./users/users-router.js');
+
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -17,13 +24,59 @@ const cors = require("cors");
 
 const server = express();
 
+
+const sessionConfig = {
+  name: 'chocolatechip', // default value is sid
+  secret: process.env.SESSION_SECRET || 'keep it safe',  // key for encryption
+  cookie: {
+    maxAge: 1000 * 60 * 10, // how long a cookie is valid for, this would last 10 mins
+    secure: process.env.USE_SECURE_COOKIES || false, // send the cookie only over https (secure connection)
+    httpOnly: true,  // prevent JS code on client from accessing THIS cookie  
+  },
+  resave: false,
+  saveUninitialized: true, // read docs, it's related to (GDPR compliance) - this a law
+  store: knexStore,
+};
+
+
+
 server.use(helmet());
-server.use(express.json());
+
 server.use(cors());
+server.use(session(sessionConfig));
+server.use(express.json());
+
+server.use('/api/auth', authRouter);
+server.use('/api/users', usersRouter);
+
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
 });
+
+
+
+// server.get("/hash", (req, res) => {
+//   const password = req.headers.authorization;
+//   const secret = req.headers.secret;
+
+//   const hash = hashString(secret)
+
+//   if (password === "melon") {
+//     res.json({ welcome: 'friend', secret, hash })
+//   } else {
+//     res.status(401).json({ Message: "you can not pass!" });
+//   }
+// });
+
+// function hashString(str) {
+//   // use bcryptjs to hash the str argument and return the hash
+//   const rounds = process.env.HASH_ROUNDS || 4;
+//   const hash = bcryptjs.hashSync(str, rounds)
+//   return hash;
+// }
+
+
 
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({
